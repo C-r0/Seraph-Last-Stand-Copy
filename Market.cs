9fb212e989a290c0df8bd15eea2ee3d4e242f9d2
C-r0 <146779565+C-r0@users.noreply.github.com>
2024-05-03 09:31:43 -0300

    Update Market.cs

--- Market.cs
+++ Market.cs
@@ -2,21 +2,21 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using UnityEditor;
 using UnityEngine;
 
 public class Market : MonoBehaviour
 {
     // Variaveis
     public GameObject mIcon; // Variavel do icone
+    public GameObject mE; // Variavel do botão E
+    public Vector3 mEpos; // Variavel da posição do botao E
+    public Vector3 mEpose; // Variavel da posição do botão E quando esta fora da cena
+    public Vector3 mEscale; // Variavel da escala do botao E
 
     void Start() // Executa ao iniciar o jogo
     {
         mIcon.SetActive(false); // Desativa o icone
-    }
-
-    void Update() // Executa a cada frame
-    {
-
     }
 
     private void OnTriggerExit2D(Collider2D other) // Ativa quando o player sai de contato com o objeto
@@ -24,6 +24,7 @@ public class Market : MonoBehaviour
         if (other.CompareTag("Player")) // Verifica se é o player
         {
             mIcon.SetActive(false); // Desativa o icone
+            mE.transform.position = mEpose; // Troca a posição do mE para a variavel mEpose
         }
     }
     private void OnTriggerEnter2D(Collider2D other) // Ativa quando o player Entra em contato com o objeto
@@ -31,6 +32,8 @@ public class Market : MonoBehaviour
         if (other.CompareTag("Player")) // Verifica se é o player
         {
             mIcon.SetActive(true); // Ativa o icone
+            mE.transform.position = mEpos; // Troca a posição do mE para a variavel mEpos
+            mE.transform.localScale = mEscale; // Troca a escala do mE para a escala mEscale
         }   
     }
 }
